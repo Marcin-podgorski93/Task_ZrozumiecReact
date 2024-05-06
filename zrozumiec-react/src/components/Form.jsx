35a75ce3b2dabc55891dc9864086a0c6585ed9a5
marcinpodgorski <podgorski.marcin93@gmail.com>
2024-05-06 12:19:13 +0200

    add submit form to grab imormation from Author and text and add new line witch submited text

--- zrozumiec-react/src/components/Form.jsx
+++ zrozumiec-react/src/components/Form.jsx
@@ -1,23 +1,51 @@
 import { useState } from "react";
 
 export function Form() {
-  //   const [review, setReview] = useState(null);
+  const [review, setReview] = useState(null);
+  const [inputValue, setInputVale] = useState("");
+  const [textareaValue, setareaVale] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(e);
+
+    const author = inputValue;
+    const text = textareaValue;
+    console.log(author);
+    console.log(text);
+    setReview({ author, text });
+  }
 
   return (
     <>
       <h1>Dodaj recenzje</h1>
-      <form>
+      {review && (
+        <>
+          <h2>{review.author}</h2>
+          <h3>{review.text}</h3>
+        </>
+      )}
+      <form onSubmit={handleSubmit}>
         <div>
           <div>
             <label htmlFor="author">Autor</label>
           </div>
-          <input type="text" name="author" id="author" />
+          <input
+            type="text"
+            name="author"
+            id="author"
+            onChange={(e) => setInputVale(e.target.value)}
+          />
         </div>
         <div>
           <div>
             <label htmlFor="text">Tekst</label>
           </div>
-          <textarea name="text" id="text"></textarea>
+          <textarea
+            name="text"
+            id="text"
+            onChange={(e) => setareaVale(e.target.value)}
+          ></textarea>
         </div>
         <button type="submit">Przeslij</button>
       </form>
