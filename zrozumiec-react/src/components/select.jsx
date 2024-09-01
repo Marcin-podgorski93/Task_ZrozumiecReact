4a77769b583504ffb85c92590cea346ca4a61a51
marcinpodgorski <podgorski.marcin93@gmail.com>
2024-09-01 23:12:26 +0200

    create table for select function

--- zrozumiec-react/src/components/select.jsx
+++ zrozumiec-react/src/components/select.jsx
@@ -1,12 +1,31 @@
+import { useState } from "react";
+
+const countries = [
+  { name: "Polska", continent: "Europa" },
+  { name: "Chiny", continent: "Azja" },
+  { name: "Kongo", continent: "Afryka" },
+  { name: "Francja", continent: "Europa" },
+  { name: "Australia", continent: "Australia" },
+  { name: "Włochy", continent: "Europa" },
+];
+
 export function Select1() {
+  const [filter, setFilter] = useState("Dowolny");
+
+  console.log(filter);
+
   return (
     <>
       <h1>Select</h1>
 
-      <select onChange={(e) => console.log(e.target.value)}>
+      <select value={filter} onChange={(e) => setFilter(e.target.value)}>
         <option value="Dowolny">Dowolny</option>
         <option value="Azja">Azja</option>
+        <option value="Europa">Europa</option>
+        <option value="Australia">Australia</option>
+        <option value="Afryka">Afryka</option>
       </select>
+      <ul></ul>
     </>
   );
 }
